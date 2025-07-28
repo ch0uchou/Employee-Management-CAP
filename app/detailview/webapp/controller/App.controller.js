d1861a9ffeefbd7c33244eb5be1ffc2ac5172bf7
Hoàng Châu <102210036@sv1.dut.udn.vn>
2025-07-28 08:21:38 +0000

    test

--- app/detailview/webapp/controller/App.controller.js
+++ app/detailview/webapp/controller/App.controller.js
@@ -5,21 +5,6 @@ sap.ui.define([
 
   return BaseController.extend("detailview.controller.App", {
       onInit() {
-          // Initialize router to navigate to DetailView automatically
-          var oRouter = this.getOwnerComponent().getRouter();
-          if (oRouter) {
-              oRouter.initialize();
-              
-              // Get component data to determine which route to navigate to
-              var oComponentData = this.getOwnerComponent().getComponentData();
-              if (oComponentData && oComponentData.employeeId) {
-                  oRouter.navTo("RouteDetailView", {
-                      employeeId: oComponentData.employeeId
-                  });
-              } else {
-                  oRouter.navTo("DetailView");
-              }
-          }
       }
   });
 });
